150a13cf4314435808e533ced48087afef0215ca
Harmaton <njagiiharmaton@gmail.com>
2022-09-02 11:02:04 +0300

    home page compose done

--- app/src/main/java/com/njagi/breakinggood/MainActivity.kt
+++ app/src/main/java/com/njagi/breakinggood/MainActivity.kt
@@ -6,18 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.njagi.breakinggood.ui.theme.BreakingGoodTheme
 import com.njagi.breakinggood.viewmodels.CharacterState
@@ -44,12 +41,12 @@ class MainActivity : ComponentActivity() {
 fun FetchData(
     characterViewModel: CharacterViewModel = viewModel()
 ){
- Column {
+ Column(modifier = Modifier.fillMaxSize()) {
      when(val state = characterViewModel.characterstate.collectAsState().value){
          is CharacterState.Empty -> Text(text = "Nothing to see")
          is CharacterState.Loading -> Text(text = "Loading...")
          is CharacterState.Success -> Text(text = "Successful")
-         is CharacterState.Error -> Text(text = "Error")
+         is CharacterState.Error -> Text(state.message)
      }
  }
 
@@ -62,10 +59,12 @@ fun AppHead(){
         verticalAlignment = Alignment.Top
         ) {
         IconButton(onClick = { /*TODO*/ }) {
-           Icon(imageVector = Icons.Default.Create, contentDescription = "create")
+           Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = "Favourites")
         }
         
-        Text(text = "Breaking Good ")
+        Text(text = "Breaking Good ",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.paddingFromBaseline(top = 20.dp))
 
         IconButton(onClick = { /*TODO*/ }) {
             Icon(imageVector = Icons.Default.Refresh, contentDescription = "refresh")
@@ -74,12 +73,15 @@ fun AppHead(){
     }
 }
 
+@Composable
+fun MainCharacter(){
+
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BreakingGoodTheme {
-    AppHead()
-        FetchData()
+        AppHead()
     }
 }
